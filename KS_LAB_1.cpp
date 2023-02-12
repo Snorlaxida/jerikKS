19716c25422a5785bf2ed950887f651467554534
Snorlaxida <106033058+Snorlaxida@users.noreply.github.com>
2023-02-12 23:04:39 +0300

    Add files via upload

---
+++ KS_LAB_1.cpp
@@ -0,0 +1,60 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#pragma warning (disable:4996)
+#include <iostream>
+#include <winsock2.h>
+
+int main()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	wVersionRequested = MAKEWORD(2, 2);
+	WSAStartup(wVersionRequested, &wsaData);
+
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in local;
+	local.sin_family = AF_INET;
+	local.sin_port = htons(1280);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
+	int c = bind(s, (struct sockaddr*)&local, sizeof(local));
+
+	int r = listen(s, 5);
+
+	//SOCKET accept(SOCKET s, struct sockaddr FAR * addr, int FAR * addrlen);
+	SOCKET s2;
+	while (true) {
+		char buf[255], res[100], b[255], * Res;
+		//структура определяет удаленный адрес,
+		//с которым  соединяется сокет
+		sockaddr_in remote_addr;
+		int size = sizeof(remote_addr);
+		s2 = accept(s, (struct sockaddr*)&remote_addr, &size);
+
+
+		while (recv(s2, b, sizeof(b), 0) != 0) {
+
+			int i = 0;
+			for (int j = 0; j <= strlen(b); j++)
+			{
+				if (((b[j] == ' ') && (b[j + 1] == ' ')) || ((b[j] == ' ') && (i == 0))) continue;
+				else
+				{
+					res[i] = b[j];
+					i++;
+				}
+			}
+			char Res[100];
+			//Res = new char[strlen(res) + 1];
+			strcpy(Res, res);
+			Res[strlen(res)] = '\0';
+
+
+			//Посылает данные на соединенный сокет
+			send(s2, Res, strlen(Res) + 1, 0);
+		}
+	}
+
+	closesocket(s2);
+	WSACleanup();
+
+}
---
+++ KS_LAB_1_CLI.cpp
@@ -0,0 +1,41 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#pragma warning (disable:4996)
+#include <winsock2.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	wVersionRequested = MAKEWORD(2, 2);
+	WSAStartup(wVersionRequested, &wsaData);
+
+	struct sockaddr_in peer;
+	peer.sin_family = AF_INET;
+	peer.sin_port = htons(1280);
+	//т.к. клиент и сервер на одном компьютере,
+	// пишем адрес 127.0.0.1
+	peer.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+
+	connect(s, (struct sockaddr*)&peer, sizeof(peer));
+
+	char buf[255], b[255];
+	cout << "Enter the string, please" << endl;
+	cin.getline(buf, 100, '\n');
+
+	send(s, buf, sizeof(buf), 0);
+	if (recv(s, b, sizeof(b), 0) != 0) {
+		/*b[strlen(b)] = '\0';*/ //Удаление ненужных символов
+						 // в конце строки	
+		cout << b << endl;
+	}
+
+	closesocket(s);
+
+	WSACleanup();
+
+	return 0;
+}
